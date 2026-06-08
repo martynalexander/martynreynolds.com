75842842ec05394709f5314dd1c937938430041f
martynalexander <martynalexander@gmail.com>
2026-06-08 21:25:32 +0200

    Update tina-config.js

--- tina-config.js
+++ tina-config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "tinacms";
 
 export default defineConfig({
   branch: "main",
-  clientId: null, // Free local/personal build
-  token: null,
+  clientId: "1524d320-7ee4-4a3e-bca7-351916403e51", // Your exact Client ID
+  token: null, // Left blank for personal deployment
   build: {
     outputFolder: "admin",
     publicFolder: "./",
@@ -17,17 +17,38 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        name: "archive",
+        name: "portfolio",
         label: "Artwork Archive",
         path: "./",
         match: {
           include: "index",
         },
+        ui: {
+          router: () => "/",
+        },
         fields: [
           {
-            type: "string",
-            name: "title",
-            label: "Main Website Title",
+            type: "object",
+            list: true,
+            name: "artworks",
+            label: "Images & Videos List",
+            ui: {
+              itemProps: (item) => {
+                return { label: item?.title || "New Artwork Entry" };
+              },
+            },
+            fields: [
+              {
+                type: "image",
+                name: "src",
+                label: "Upload or Select File",
+              },
+              {
+                type: "string",
+                name: "title",
+                label: "Artwork Information Line (Use | slashes)",
+              },
+            ],
           },
         ],
       },
